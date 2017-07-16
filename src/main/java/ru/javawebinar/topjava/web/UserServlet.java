92e2ddb5abf02597a191c322def79c08ee3f4cb7
Ilya Semenov <semenovio@gmail.com>
2017-07-17 00:35:14 +0300

    1 5 forward to redirect

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,14 +1,16 @@
 package ru.javawebinar.topjava.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by Ilya on 16.07.2017.
- */
-public class UserServlet extends javax.servlet.http.HttpServlet {
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        //request.getRequestDispatcher("users.jsp").forward(request, response);
-        //
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
     }
 }
