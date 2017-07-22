8656cf7cca1f120463067acbd2a32f502b5cceac
Ilya Semenov <semenovio@gmail.com>
2017-07-23 02:06:08 +0300

    HW2 before changes

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryMealRepositoryImpl.java
@@ -0,0 +1,45 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.repository.MealRepository;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+@Repository
+public class InMemoryMealRepositoryImpl implements MealRepository {
+    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        MealsUtil.MEALS.forEach(this::save);
+    }
+
+    public Meal save(Meal meal) {
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+        }
+        repository.put(meal.getId(), meal);
+        return meal;
+    }
+
+    @Override
+    public void delete(int id) {
+        repository.remove(id);
+    }
+
+    @Override
+    public Meal get(int id) {
+        return repository.get(id);
+    }
+
+    @Override
+    public Collection<Meal> getAll() {
+        return repository.values();
+    }
+}
+
---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.service.MealService;
+
+@Controller
+public class MealRestController {
+    private MealService service;
+
+}
