5d1518e19adefca5869b1118c0892f9d6c1ac24a
Ilya Semenov <semenovio@gmail.com>
2017-07-24 02:49:44 +0300

    HW2 Done Tasks 1 and 2

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,66 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.NamedEntity;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+    private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private Map<Integer, User> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return true;
+    }
+
+    @Override
+    public User save(User user) {
+        log.info("save {}", user);
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        log.info("get {}", id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        log.info("getAll");
+        return repository.values().stream()
+                .sorted(Comparator.comparing(NamedEntity::getName))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        log.info("getByEmail {}", email);
+//        for (User user : repository.values()) {
+//            if (email.equals(user.getEmail()))
+//                return user;
+//        }
+        List<User> filteredByEmail = repository.values().stream()
+                                            .filter(user -> email.equals(user.getEmail()))
+                                            .collect(Collectors.toList());
+        if (filteredByEmail.size() == 0)
+            return null;
+        else
+            return filteredByEmail.get(0);
+    }
+}
