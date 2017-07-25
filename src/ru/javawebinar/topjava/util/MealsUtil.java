8a5039be64d0a77eda22ecc475990e767f4c87d6
Ilya Semenov <semenovio@gmail.com>
2017-07-26 01:47:52 +0300

    correct master branch 0.1

--- src/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/ru/javawebinar/topjava/util/MealsUtil.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.util;
 
-import ru.javawebinar.topjava.model.UserMeal;
-import ru.javawebinar.topjava.model.UserMealWithExceed;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -9,26 +9,22 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 
-/**
- * GKislin
- * 31.05.2015.
- */
-public class UserMealsUtil {
+public class MealsUtil {
     public static void main(String[] args) {
-        List<UserMeal> mealList = Arrays.asList(
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        List<Meal> mealList = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
-    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+    public static List<MealWithExceed>  getFilteredWithExceeded(List<Meal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // returns filtered list with correctly exceeded field
         Map<LocalDate, Integer> caloriesCounter = new HashMap<>();
         List<UserMealWithExceed> filteredMealList = new ArrayList<>();
